ac167d55d07a9236766089903995bfb8b8dc8ce6
Ilia Choly <icholy@accipiterradar.com>
2017-02-23 13:00:18 -0500

    add more control over debug drawwing

--- replacer/replacer.go
+++ replacer/replacer.go
@@ -14,7 +14,12 @@ import (
 var haarCascade = flag.String("haar", "haarcascade_frontalface_alt.xml", "The location of the Haar Cascade XML configuration to be provided to OpenCV.")
 var margin = flag.Float64("margin", 50.0, "The face rectangle margin")
 var showRects = flag.Bool("show.rects", false, "Show the detection rectangles")
+var drawFace = flag.Bool("draw.face", true, "Draw the face")
 var faceOpacity = flag.Float64("face.opacity", 1.0, "Face opacity [0-255]")
+
+var showRectDetection = flag.Bool("show.rects.detection", true, "Show the detection rects")
+var showRectPadding = flag.Bool("show.rects.padding", true, "Show the padding rects")
+var showRectPlacement = flag.Bool("show.rects.placement", true, "Show the placement rects")
 
 type FaceReplacer struct {
 	base   image.Image
@@ -63,26 +68,34 @@ func (fr *FaceReplacer) AddFaces() (*image.NRGBA, error) {
 		blue  = color.RGBA{0, 0, 255, 255}
 	)
 
-	for _, rect := range fr.rects {
+	for _, faceRect := range fr.rects {
 
 		srcFaceImg := fr.faces.Random()
 
 		// add padding around detected face rect
-		paddedRect := addRectPadding(*margin, rect)
+		paddingRect := addRectPadding(*margin, faceRect)
 
 		// resize the face image to fit inside the padded rect
-		faceImg := imaging.Fit(srcFaceImg, paddedRect.Dx(), paddedRect.Dy(), imaging.Lanczos)
+		faceImg := imaging.Fit(srcFaceImg, paddingRect.Dx(), paddingRect.Dy(), imaging.Lanczos)
 
 		// center the face rect size inside the padded rect
-		faceRect := getRectCenteredIn(faceImg.Rect, paddedRect)
+		placementRect := getRectCenteredIn(faceImg.Rect, paddingRect)
 
 		// draw the face
-		canvas = imaging.Overlay(canvas, faceImg, faceRect.Min, *faceOpacity)
+		if *drawFace {
+			canvas = imaging.Overlay(canvas, faceImg, placementRect.Min, *faceOpacity)
+		}
 
 		if *showRects {
-			drawRect(canvas, rect, red)
-			drawRect(canvas, paddedRect, green)
-			drawRect(canvas, faceRect, blue)
+			if *showRectDetection {
+				drawRect(canvas, faceRect, red)
+			}
+			if *showRectPadding {
+				drawRect(canvas, paddingRect, green)
+			}
+			if *showRectPadding {
+				drawRect(canvas, placementRect, blue)
+			}
 		}
 	}
 
