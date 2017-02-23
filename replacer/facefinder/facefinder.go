ea1324454ccf54995a0ea4d3ed53b309dfda444b
Ilia Choly <ilia.choly@gmail.com>
2017-02-22 22:11:50 -0500

    make minNeighboor parameter a flag

--- replacer/facefinder/facefinder.go
+++ replacer/facefinder/facefinder.go
@@ -1,10 +1,13 @@
 package facefinder
 
 import (
+	"flag"
 	"image"
 
 	"github.com/lazywei/go-opencv/opencv"
 )
+
+var minNeighboor = flag.Int("min.neighboor", 5, "the lower this number is, the more faces will be found")
 
 var faceCascade *opencv.HaarCascade
 
@@ -25,7 +28,7 @@ func (f *Finder) Close() {
 func (f *Finder) Detect(i image.Image) []image.Rectangle {
 	var output []image.Rectangle
 
-	faces := f.cascade.DetectObjects(opencv.FromImage(i), 5)
+	faces := f.cascade.DetectObjects(opencv.FromImage(i), *minNeighboor)
 	for _, face := range faces {
 		output = append(output, image.Rectangle{
 			image.Point{face.X(), face.Y()},
