6385edeed0c68852e3bc14f12c5aa547b13c49c4
Ilia Choly <icholy@accipiterradar.com>
2017-02-27 10:11:30 -0500

    move some functions out of main.go

--- main.go
+++ main.go
@@ -1,21 +1,15 @@
 package main
 
 import (
-	"bufio"
 	"encoding/json"
 	"flag"
 	"fmt"
-	"image"
 	"image/jpeg"
 	_ "image/png"
-	"io"
-	"io/ioutil"
 	"log"
-	"math/rand"
 	"net/http"
 	_ "net/http/pprof"
 	"os"
-	"path/filepath"
 	"time"
 
 	"github.com/jasonlvhit/gocron"
@@ -69,44 +63,6 @@ func (t *times) Set(value string) error {
 	return nil
 }
 
-func testImage(imgfile string, w io.Writer) error {
-	f, err := os.Open(imgfile)
-	if err != nil {
-		return err
-	}
-	defer f.Close()
-	baseImage, _, err := image.Decode(f)
-	if err != nil {
-		return err
-	}
-	faces := faceutil.DetectFaces(baseImage)
-	log.Printf("found %d face(s) in image\n", len(faces))
-	newImage := faceutil.DrawFaces(baseImage, faces)
-	return jpeg.Encode(w, newImage, &jpeg.Options{Quality: jpeg.DefaultQuality})
-}
-
-func testImageDir(dir string) error {
-	entries, err := ioutil.ReadDir(dir)
-	if err != nil {
-		return err
-	}
-	for _, e := range entries {
-		var (
-			srcFile = filepath.Join(dir, e.Name())
-			dstFile = filepath.Join(dir, "nick_"+e.Name())
-		)
-		f, err := os.Create(dstFile)
-		if err != nil {
-			return err
-		}
-		defer f.Close()
-		if err := testImage(srcFile, f); err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
 func main() {
 	flag.Parse()
 
@@ -141,7 +97,7 @@ func main() {
 		return
 	}
 
-	captions, err := readCaptions("captions.txt")
+	captions, err := readLines("captions.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -161,35 +117,7 @@ func main() {
 	go bot.Run()
 
 	if *httpport != "" {
-		http.HandleFunc("/demo", func(w http.ResponseWriter, r *http.Request) {
-			img, err := bot.Demo()
-			if err != nil {
-				http.Error(w, err.Error(), http.StatusInternalServerError)
-				return
-			}
-			w.Header().Add("Content-Type", "image/jpeg")
-			if err := jpeg.Encode(w, img, &jpeg.Options{Quality: jpeg.DefaultQuality}); err != nil {
-				http.Error(w, err.Error(), http.StatusInternalServerError)
-				return
-			}
-		})
-		http.HandleFunc("/stats", func(w http.ResponseWriter, r *http.Request) {
-			stats, err := store.Stats(model.MediaAvailable)
-			if err != nil {
-				http.Error(w, err.Error(), http.StatusInternalServerError)
-				return
-			}
-			w.Header().Add("Content-Type", "application/json")
-			if err := json.NewEncoder(w).Encode(stats); err != nil {
-				http.Error(w, err.Error(), http.StatusInternalServerError)
-				return
-			}
-		})
-		go func() {
-			if err := http.ListenAndServe(*httpport, nil); err != nil {
-				log.Printf("error: %s\n", err)
-			}
-		}()
+		startHTTPServer(bot, store)
 	}
 
 	doPost := func() {
@@ -217,26 +145,34 @@ func main() {
 	}
 }
 
-func shuffle(slice []string) {
-	for i := range slice {
-		j := rand.Intn(i + 1)
-		slice[i], slice[j] = slice[j], slice[i]
-	}
-}
-
-func readCaptions(filename string) ([]string, error) {
-	f, err := os.Open(filename)
-	if err != nil {
-		return nil, err
-	}
-	defer f.Close()
-	var captions []string
-	scanner := bufio.NewScanner(f)
-	for scanner.Scan() {
-		captions = append(captions, scanner.Text())
-	}
-	if err := scanner.Err(); err != nil {
-		return nil, err
-	}
-	return captions, err
+func startHTTPServer(bot *facebot.Bot, store *imgstore.Store) {
+	http.HandleFunc("/demo", func(w http.ResponseWriter, r *http.Request) {
+		img, err := bot.Demo()
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.Header().Add("Content-Type", "image/jpeg")
+		if err := jpeg.Encode(w, img, &jpeg.Options{Quality: jpeg.DefaultQuality}); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	})
+	http.HandleFunc("/stats", func(w http.ResponseWriter, r *http.Request) {
+		stats, err := store.Stats(model.MediaAvailable)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.Header().Add("Content-Type", "application/json")
+		if err := json.NewEncoder(w).Encode(stats); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	})
+	go func() {
+		if err := http.ListenAndServe(*httpport, nil); err != nil {
+			log.Printf("error: %s\n", err)
+		}
+	}()
 }
